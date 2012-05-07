2a71cc2a573b624974172988fc74b64b915f061c
Jeff Chen <rememberthebaldness@gmail.com>
2012-05-07 13:49:45 -0700

    Commented code and made revisions that reflect those in analog.h

--- src/analog.cpp
+++ src/analog.cpp
@@ -1,7 +1,6 @@
 #include "analog.h"
 
-AnalogInput::AnalogInput(PinName pin, float thresh) {
-	threshold = thresh;
+AnalogInput::AnalogInput(PinName pin) {
 	p_analog = new AnalogIn(pin);
 }
 
@@ -11,16 +10,14 @@ AnalogInput::~AnalogInput() {
 	}
 }
 
+/*
+ * Returns the raw value of the device. Probably noisy.
+ */
 float AnalogInput::getValueRaw() {
 	return p_analog->read();
 }
 
-bool AnalogInput::getValueThresh() {
-	return (p_analog->read() > threshold);
-}
-
-AnalogPressureSensor::
-AnalogPressureSensor(PinName pin, float calibration_m, float calibration_b) : 
+AnalogPressureSensor::AnalogPressureSensor(PinName pin, float calibration_m, float calibration_b) : 
 AnalogInput(pin, 0.5f) {
 	m = calibration_m;
 	b = calibration_b;
@@ -30,20 +27,26 @@ AnalogPressureSensor::~AnalogPressureSensor() {
 	
 }
 
+
+/*
+ *Returns the current depth of the submarine, as determined by the linear equation
+ * with parameters m and b (y = mx + b)
+ * x is the raw pressure sensor reading (voltage)
+ * y = calibrated pressure sensor reading (feet)
+ * We add 0.5 to y so that it rounds correctly when we truncate into an integer.
+ */
 int AnalogPressureSensor::getValueCalibrated() {
-	// This is a y = mx + b equation.
-	// x = raw pressure sensor reading (voltage)
-	// y = calibrated pressure sensor reading (feet)
-	// We're adding 0.5 so that it rounds correctly when we truncate into an int.
+
 	return (int)(getValueRaw() * m + b + 0.5f);
 }
 
-AnalogKillSwitch::
-AnalogKillSwitch(PinName Vin, PinName Vout, float thresh) :
-AnalogInput(Vin, thresh) {
+AnalogKillSwitch::AnalogKillSwitch(PinName Vin, PinName Vout, float thresh) :
+AnalogInput(Vin) {
+	threshold = thresh;
 	p_Vout = new AnalogOut(Vout);
 	p_Vout->write(1.0f); // 1.0 * 3.3 volts
 }
+
 AnalogKillSwitch::~AnalogKillSwitch() {
 	// TODO: does the AnalogOut class automatically change the voltage to 0 when
 	// it gets destroyed?
@@ -53,12 +56,20 @@ AnalogKillSwitch::~AnalogKillSwitch() {
 	}
 }
 
+/* 
+ * getValueRaw is a slight misnomer, considering that we average the raw value
+ * with previous values. In fact, it's unlikely that we need this code; there
+ * shouldn't be that much noise.
+ */
 float AnalogKillSwitch::getValueRaw() {
 	// TODO: test this averaging code. Is it enough?
 	value = (AnalogInput::getValueRaw() + value) / 2;
 	return value;
 }
 
+/* 
+ * Returns true if the raw value exceeds the threshold passed into the constructor
+ */
 bool AnalogKillSwitch::getValueThresh() {
-	return (value > threshold);
+	return (getValueRaw() > threshold);
 }
