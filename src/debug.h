d428bb6af0486598e4c10c27b44e9f0507d13f06
Jeff Chen <rememberthebaldness@gmail.com>
2012-05-07 13:54:03 -0700

    Commented code.

--- src/debug.h
+++ src/debug.h
@@ -1,18 +1,23 @@
-
 #include "mbed.h"
 #include <stdarg.h>
 
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
-//attempts to print a string to serial
 
+/*
+ * Prints to a serial port. Max string size is 255.
+ */
 void inline print_serial(Serial* serial, const char* format, ...) {
 	char buffer[256];
 	va_list args;
 	va_start (args, format);
 	vsprintf(buffer, format, args);
-	buffer[255] = '\0';
+	buffer[255] = '\0';	//Ensure that the buffer is null terminated.
+	
+	/*
+	 * We print one character at a time because we drop characters otherwise.
+	 */
 	for (int i = 0; buffer[i] != '\0'; i++){
 		while (!serial->writeable());
 		serial->putc(buffer[i]);
@@ -20,6 +25,7 @@ void inline print_serial(Serial* serial, const char* format, ...) {
 	va_end(args);
 }
 
+//Lights!
 extern DigitalOut led1, led2, led3, led4;
 
 #endif
