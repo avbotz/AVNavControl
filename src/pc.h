fd5c8c3143b310772a7187b03ca14438b4fb886c
Jeff Chen <rememberthebaldness@gmail.com>
2012-05-08 08:54:41 -0700

    Formatting

--- src/pc.h
+++ src/pc.h
@@ -15,13 +15,16 @@ avnav encode_avnav(int data);
 int decode_avnav(avnav data);
 void send_to_pc();
 
-// This is the buffer for the message that will be sent to the BeagleBoard.
-// Format: 'h', AVNav-encoded data for heading,
-//         'd', AVNav-encoded data for depth (pressure sensor),
-//         'p', AVNav-encoded data for power (motors),
-//         kill switch status (l is dead and k is alive),
-//         '\n' to terminate the message.
+/*
+ * This is the buffer for the message that will be sent to the BeagleBoard.
+ * Format: 'h', AVNav-encoded data for heading,
+ *         'd', AVNav-encoded data for depth (pressure sensor),
+ *         'p', AVNav-encoded data for power (motors),
+ *         kill switch status (l is dead and k is alive),
+ *         '\n' to terminate the message.
+ */
 char mes[] = "h||d||p|||\n";
+
 #define MESSAGE_LENGTH (sizeof(mes)/sizeof(char))
 
 
