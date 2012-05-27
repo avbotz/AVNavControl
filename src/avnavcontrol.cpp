avbotz/AVNavControl

commit 805e77c241ae39da2868b1e0478c15745776bcce

I put the interrupts in the correct classes so now the motor interrupt is in the Motor class so it can be attached in the constructor and stuff
Also I make the PC a class that has circular TX and RX FIFO's.  It
attaches the ticker and interrupts and sends the status to the beagleboard
and receives the desired stuff.

And I updated the main function to be compatible with this

src/avnavcontrol.cpp
@@ -7,22 +7,9 @@ bool manual = false;
 
 unsigned char motorArray[4];
 
-Serial asdf(p28, p27);
 
 int main() {
-	// Set BeagleBoard's baud to 115200.
-	pc.baud(115200);
-	asdf.baud(9600);
-	asdf.format(8, Serial::None, 1);
-	// Attach a callback so that readIMU() is called when the IMU gets a character.
-	imu.attach(&readIMU);
-	imu.p_device->putc('4');	// tell the imu to start sending in case it isn't doing that already.
-	// Set the motors to no power.
-	motor.set(127);
-	// Attach a callback for sending the motor.
-	motor.attach(&sendMotor);
-	// Set an interrupt to send data to the BeagleBoard every 1.0 seconds.
-	ticker_pc.attach(&sendPC, 1.0);
+
 	//
 	init_pid();
 
@@ -40,126 +27,118 @@ int main() {
 		//led1 = isAlive = kill.getValueThresh();
 		//led2 = !led2;
 		if (true) {
+
+			if (!debug) {
+				pc.readPC();
+				desHead = pc.desired_heading;
+				desPower = pc.desired_power;
+				desDepth = pc.desired_depth;
+			}
+
 			imu.getData();
-			//pc.printf("got data\n\r");
 	
 			led3 = !led3;
 
 			if (!manual) {
 				give_data(imu.accX, imu.accY, imu.accZ, imu.gyrX, imu.gyrY, imu.gyrZ);
 				do_pid();
-				//pc.printf("Motor speeds:	");
 				for (int i = 0; i < 4; i++) {
-					//sprintf(filler, "%d\t", motorArray[i]);
 					motor.set(i, motorArray[i]);
-					//pc.printf("%d\t", motorArray[i]);
 				}
 			}
 			if (motor.buffer_empty) {
-				sendMotor();
+				motor.tx_interrupt();
+			}
+			if (pc.tx_empty) {
+				pc.tx_interrupt();
 			}
 	
-			while (pc.readable()) {
-				// TODO: this should be used to handle message from AVI
-				// Format for messages from AVI is com prefix, data byte 1, data byte 2, '\n'
-				// where com prefix is h for desired heading, d for desired depth,
-				// p for desired power
-				manual = true;
+			if (debug) {
 				char in;
-				in = pc.getc();
-				switch (in) {
-					case 'g':		// All motors
-						motor.set(LEFT, 0);
-						motor.set(RIGHT, 0);
-						motor.set(FRONT, 0);
-						motor.set(BACK, 254);
+				while (in = pc.readPC()) {
+					// TODO: this should be used to handle message from AVI
+					// Format for messages from AVI is com prefix, data byte 1, data byte 2, '\n'
+					// where com prefix is h for desired heading, d for desired depth,
+					// p for desired power
+					manual = true;
+					switch (in) {
+						case 'g':		// All motors
+							motor.set(LEFT, 0);
+							motor.set(RIGHT, 0);
+							motor.set(FRONT, 0);
+							motor.set(BACK, 254);
+							break;
+						case 'f':		// Forward
+							motor.set(LEFT, 0);
+							motor.set(RIGHT, 0);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 127);
+							break;
+						case 'b':		// Back
+							motor.set(LEFT, 254);
+							motor.set(RIGHT, 254);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 127);
+							break;
+						case 'u':		// Up
+							motor.set(LEFT, 127);
+							motor.set(RIGHT, 127);
+							motor.set(FRONT, 254);
+							motor.set(BACK, 0);
+							break;
+						case 'd':		// Down
+							motor.set(LEFT, 127);
+							motor.set(RIGHT, 127);
+							motor.set(FRONT, 0);
+							motor.set(BACK, 254);
+							break;
+						case 'r':		// Turn right
+							motor.set(LEFT, 0);
+							motor.set(RIGHT, 254);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 127);
+							break;
+						case 'l':		// Turn left
+							motor.set(LEFT, 254);
+							motor.set(RIGHT, 0);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 127);
+							break;
+						case 'z':		// Debug; runs the back motor
+							motor.set(LEFT, 127);
+							motor.set(RIGHT, 127);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 254);
+							break;
+						case 'y':		// Debug: runs all but the left motor
+							motor.set(LEFT, 255);
+							motor.set(RIGHT, 0);
+							motor.set(FRONT, 0);
+							motor.set(BACK, 0);
+							break;
+						case 's':		// Stops the motors immediately
+							motor.set(LEFT, 127);
+							motor.set(RIGHT, 127);
+							motor.set(FRONT, 127);
+							motor.set(BACK, 127);
+								break;
+						case 'i':
+							manual = false;
+							//print info and stuff
+							break;
+						case '\e': // escape
+							manual = false;
+							break;
+						case '9':	// Act as a passthrough between IMU and PC
+							imu.directAccess();
+							manual = false;
+							break;
+						default:
+							pc.printf("Unrecognized command.\n\r");
 						break;
-					case 'f':		// Forward
-						motor.set(LEFT, 0);
-						motor.set(RIGHT, 0);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 127);
-						break;
-					case 'b':		// Back
-						motor.set(LEFT, 254);
-						motor.set(RIGHT, 254);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 127);
-						break;
-					case 'u':		// Up
-						motor.set(LEFT, 127);
-						motor.set(RIGHT, 127);
-						motor.set(FRONT, 254);
-						motor.set(BACK, 0);
-						break;
-					case 'd':		// Down
-						motor.set(LEFT, 127);
-						motor.set(RIGHT, 127);
-						motor.set(FRONT, 0);
-						motor.set(BACK, 254);
-						break;
-					case 'r':		// Turn right
-						motor.set(LEFT, 0);
-						motor.set(RIGHT, 254);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 127);
-						break;
-					case 'l':		// Turn left
-						motor.set(LEFT, 254);
-						motor.set(RIGHT, 0);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 127);
-						break;
-					case 'z':		// Debug; runs the back motor
-						motor.set(LEFT, 127);
-						motor.set(RIGHT, 127);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 254);
-						break;
-					case 'y':		// Debug: runs all but the left motor
-						motor.set(LEFT, 255);
-						motor.set(RIGHT, 0);
-						motor.set(FRONT, 0);
-						motor.set(BACK, 0);
-						break;
-					case 's':		// Stops the motors immediately
-						motor.set(LEFT, 127);
-						motor.set(RIGHT, 127);
-						motor.set(FRONT, 127);
-						motor.set(BACK, 127);
-						break;
-					case 'i':
-						manual = false;
-						//print info and stuff
-						break;
-					case '\e': // escape
-						manual = false;
-						break;
-					case '9':	// Act as a passthrough between IMU and PC
-						imu.directAccess();
-						manual = false;
-						break;
-					default:
-						pc.printf("Unrecognized command.\n\r");
-						break;
-	
+					}
 				}
-	
-				//read stuff here
-				//PCreadable = false;
-				//NVIC_EnableIRQ(UART0_IRQn);
 			}
-			
-			// Update the BeagleBoard with data.
-			if (PCsendable) {
-				NVIC_DisableIRQ(UART0_IRQn);
-				send_to_pc();
-				PCsendable = false;
-				NVIC_EnableIRQ(TIMER3_IRQn);
-				NVIC_EnableIRQ(UART0_IRQn);
-				//__enable_irq();
-			}
-			
 	
 		}
 		else {
@@ -169,49 +148,3 @@ int main() {
 
 }
 
-// This interrupt is called when there is a character to be read from the PC. To
-// avoid a crash or livelock, we disable interrupts at the end of the interrupt.
-// Then, in the main loop, we read everything from the buffer.
-void readPC() {
-	//PCreadable = true;
-	//NVIC_DisableIRQ(UART0_IRQn);
-}
-
-// This interrupt is called when there is a character to be read from the IMU.
-void readIMU() {
-	led2 = !led2;
-	
-	// TODO: it might be called and there are multiple characters to be read
-	while (imu.p_device->readable()) {
-		imu.buffer[imu.i_buffer_write] = imu.getc();
-		//pc.putc(imu.buffer[imu.i_buffer_write]);
-		NVIC_DisableIRQ(UART3_IRQn);
-		imu.i_buffer_write = (imu.i_buffer_write + 1) % IMU_RX_BUFFER_SIZE;
-		NVIC_EnableIRQ(UART3_IRQn);
-		if (imu.i_buffer_write == imu.i_buffer_read) {
-			imu.buffer_overflow = true;
-			NVIC_DisableIRQ(UART3_IRQn);
-			break;
-		}
-	}
-}
-
-void sendPC() {
-	//PCsendable = true;
-	//NVIC_DisableIRQ(TIMER3_IRQn);
-	//__disable_irq();
-}
-
-void sendMotor() {
-	while (motor.p_device->writeable() && motor.i_buffer_write != motor.i_buffer_read) {
-		motor.p_device->putc(motor.buffer[motor.i_buffer_read]);
-		motor.i_buffer_read = (motor.i_buffer_read + 1) % MOTOR_TX_BUF_SIZE;
-		motor.buffer_empty = false;
-	}
-	if (motor.i_buffer_write == motor.i_buffer_read) {
-		motor.buffer_empty = true;
-		NVIC_DisableIRQ(UART1_IRQn);
-		// if nothing to write, turn off the interrupt until motor.getc() is called again
-	}
-	
-}

src/avnavcontrol.h
@@ -16,11 +16,10 @@ void readIMU(), readPC(), sendPC(), sendMotor();
 
 extern Serial pc;
 
-Ticker ticker_pc;
-
 bool isAlive = false;
 bool PCreadable, PCsendable;
 
+extern bool debug;
 
 
 // Do not remove

src/global.cpp
@@ -24,7 +24,7 @@ AnalogPressureSensor pressure(p17, 100.0f, 0.0f);
 
 
 //avnavcontrol.h
-Serial pc(USBTX, USBRX); //tx, rx
+PC pc(USBTX, USBRX, 115200); //tx, rx
 
 //IMU.h
 IMU imu(p9, p10, 57600, &pc);
@@ -36,3 +36,5 @@ DigitalOut led1(LED1),	// Kill status
            led2(LED2),	// debug
            led3(LED3),	// debug
            led4(LED4);	// debug
+
+bool debug = false;

src/imu.cpp
@@ -18,8 +18,7 @@ IMU::IMU(PinName tx, PinName rx, int baud, Serial* pc) {
 	accX = accY = accZ = gyrX = gyrY = gyrZ = magX = magY = magZ = 0;
 	parseNow = false;
 	
-	debugMode = false;
-	
+	//intialize the buffers and buffer indicies
 	for (int i = 0; i < IMU_RX_BUFFER_SIZE; i++) {
 		buffer[i] = 0;
 	}
@@ -31,7 +30,9 @@ IMU::IMU(PinName tx, PinName rx, int baud, Serial* pc) {
 	i_linebuf = 0;
 	buffer_overflow = false;
 	
-	//p_device->putc('4');	// tell the imu to start sending in case it isn't doing that already.
+	// Attach it as an RX interrupt only.
+	p_device->attach(&rx_interrupt, Serial::RxIrq);
+	p_device->putc('4');	// tell the imu to start sending in case it isn't doing that already.
 }
 
 IMU::~IMU() {
@@ -77,17 +78,6 @@ bool IMU::readable() {
 
 char IMU::getc() {
 	return p_device->getc();
-}
-
-/* 
- * Wrapper function to attach a callback function to the RX interrupt of the 
- * IMU Serial object. The function is called whenever the IMU sends a 
- * character.
- */
-void IMU::attach(void (*fptr)(void)) {
-	//p_pc->putc('a');
-	// Attach it as an RX interrupt only.
-	p_device->attach(fptr, Serial::RxIrq);
 }
 
 void IMU::getData() {
@@ -209,4 +199,22 @@ void IMU::calcHeading() {
 	*/
 	
 		
+}
+
+void IMU::rx_interrupt()
+{
+	led2 = !led2;
+	
+	while (p_device->readable()) {
+		buffer[i_buffer_write] = p_device->getc();
+		//pc.putc(imu.buffer[imu.i_buffer_write]);
+		NVIC_DisableIRQ(UART3_IRQn);
+		i_buffer_write = (i_buffer_write + 1) % IMU_RX_BUFFER_SIZE;
+		NVIC_EnableIRQ(UART3_IRQn);
+		if (i_buffer_write == i_buffer_read) {
+			buffer_overflow = true;
+			NVIC_DisableIRQ(UART3_IRQn);
+			break;
+		}
+	}
 }

src/imu.h
@@ -16,9 +16,7 @@ public:
 	~IMU();
 	
 	void putc(char);
-	char getc();
 	bool readable();
-	void attach(void (*fptr)(void));
 	void parse();
 	void getData();
 	void directAccess();
@@ -27,13 +25,7 @@ public:
 	// ticks per degree, from <http://www.sparkfun.com/datasheets/Sensors/Gyro/PS-ITG-3200-00-01.4.pdf>
 	static const float gyroScale = 0.069565f;
 	static const float radToDeg = 57.29577951308232f; // equals 180/pi
-	
-	// Stores a line of raw data from the IMU
-	char buffer[IMU_RX_BUFFER_SIZE];
-	int i_buffer_read, i_buffer_write;
-	char linebuf[1024];
-	int i_linebuf;
-	bool buffer_overflow;
+
 	
 	short accX, accY, accZ, 
 		  gyrX, gyrY, gyrZ, 
@@ -41,7 +33,6 @@ public:
 	unsigned short heading; // 0 to 359. current compass heading set by calcHeading()
 	
 	bool parseNow;
-	bool debugMode;
 	
 	Serial* p_device;
 	Serial* p_pc;
@@ -54,6 +45,15 @@ private:
 			  sumMagX, sumMagY, sumMagZ;
 	int minGyrX, maxGyrX, minGyrY, maxGyrY, minGyrZ, maxGyrZ;
 	int num;
+
+	// Stores a line of raw data from the IMU
+	char buffer[IMU_RX_BUFFER_SIZE];
+	int i_buffer_read, i_buffer_write;
+	char linebuf[1024];
+	int i_linebuf;
+	bool buffer_overflow;
+
+	void rx_interupt();
 };
 
 extern IMU imu;

src/motor.cpp
@@ -13,6 +13,8 @@ Motor::Motor(int num_motors, int baud, PinName tx, PinName rx) {
 	p_device->format(8, Serial::None, 1);
 	p_device->baud(baud); // Set the baud.
 	
+	p_device->attach(&tx_interrupt, Serial::TxIrq);
+
 	for (int i = 0; i < MOTOR_TX_BUF_SIZE; i++) {
 		buffer[i] = 0;
 	}
@@ -27,11 +29,6 @@ Motor::~Motor() {
 		// must do this. otherwise, you'll have memory leakage & you may not be able to re-open the serial port later
 		delete p_device;
 	}
-}
-
-void Motor::attach(void (*fptr)(void)) {
-	// Attach as a transmit interrupt only.
-	p_device->attach(fptr, Serial::TxIrq);
 }
 
 void Motor::putc(char c) {
@@ -70,6 +67,19 @@ void Motor::set(int i_motor, unsigned char value) {
 
 char Motor::get(int i_motor) {
 	return motors[i_motor];
+}
+
+void Motor::tx_interrupt(){
+	while (p_device->writeable() && i_buffer_write != i_buffer_read) {
+		p_device->putc(buffer[i_buffer_read]);
+		i_buffer_read = (i_buffer_read + 1) % MOTOR_TX_BUF_SIZE;
+		buffer_empty = false;
+	}
+	if (i_buffer_write == i_buffer_read) {
+		buffer_empty = true;
+		NVIC_DisableIRQ(UART0_IRQn);
+		// if nothing to write, turn off the interrupt until motor.getc() is called again
+	}
 }
 
 

src/motor.h
@@ -25,7 +25,6 @@ class Motor {
 public:
 	Motor(int num_motors, int baud, PinName tx, PinName rx);
 	~Motor();
-	void attach(void (*fptr)(void));
 	void putc(char c); // add c to the FIFO buffer.
 	void send(); // send all motors' speeds to the controller
 	void send(int i_motor); // send speed of motor i_motor to the controller
@@ -35,10 +34,9 @@ public:
 	
 	Serial* p_device; // pointer to our serial object (used to send stuff)
 	
-	char buffer[MOTOR_TX_BUF_SIZE];
-	int i_buffer_read, i_buffer_write;
-	
 	bool buffer_empty;
+
+	void tx_interrupt();
 	
 private:
 	int num_motors; // number of motors, counting from 1
@@ -54,6 +52,11 @@ private:
 	 */
 
 	char motors[12]; //The controller supports twelve motors. This array locally stores their speeds.
+
+	
+
+	char buffer[MOTOR_TX_BUF_SIZE];
+	int i_buffer_read, i_buffer_write;
 };
 
 extern Motor motor;

commit 907438bcafd60202d2acf0126790c83358a566eb

Fix compile errors introduced in 805e77c241. Thanks, Adit.
- Attaching member function of a class to interrupt: fixed the problem of
passing the function pointer by making the function global and friending it in
the class.

- Assorted changes to make other parts of program work with the PC class.

- Annotate global.cpp to connect UART numbers with pin numbers.

src/avnavcontrol.cpp
@@ -22,7 +22,7 @@ int main() {
 	// every 1s: send data to pc
 	// upon serial from pc: set desired heading and desired depth
 	//print_serial(&pc, "hi");
-	int i = 0;
+	
 	while (true) {
 		//led1 = isAlive = kill.getValueThresh();
 		//led2 = !led2;
@@ -47,10 +47,10 @@ int main() {
 				}
 			}
 			if (motor.buffer_empty) {
-				motor.tx_interrupt();
+				tx_interrupt_motor();
 			}
 			if (pc.tx_empty) {
-				pc.tx_interrupt();
+				tx_interrupt_pc();
 			}
 	
 			if (debug) {
@@ -134,7 +134,7 @@ int main() {
 							manual = false;
 							break;
 						default:
-							pc.printf("Unrecognized command.\n\r");
+							pc.send_message("Unrecognized command.\n\r");
 						break;
 					}
 				}

src/avnavcontrol.h
@@ -10,11 +10,12 @@
 #include "motor.h"
 #include "pid.h"
 #include "debug.h"
+#include "pc.h"
 
 extern void send_to_pc();
 void readIMU(), readPC(), sendPC(), sendMotor();
 
-extern Serial pc;
+extern PC pc;
 
 bool isAlive = false;
 bool PCreadable, PCsendable;

src/global.cpp
@@ -8,6 +8,7 @@
 #include "analog.h"
 #include "imu.h"
 #include "motor.h"
+#include "pc.h"
 
 //analoginput.h
 // TODO: check the magic threshold 0.85f. Too high/low?
@@ -22,14 +23,18 @@ AnalogKillSwitch kill(p16, p18, 0.85f); // Vin, Vout
  */
 AnalogPressureSensor pressure(p17, 100.0f, 0.0f);
 
+// UART mapping http://mbed.org/users/mbed714/notebook/uart-mapping/
 
 //avnavcontrol.h
+// USB = UART 0
 PC pc(USBTX, USBRX, 115200); //tx, rx
 
 //IMU.h
+// pins 9, 10 = UART 3
 IMU imu(p9, p10, 57600, &pc);
 
 //motor.h
+// pins 13, 14 = UART 1
 Motor motor(4, 9600, p13, p14);
 
 DigitalOut led1(LED1),	// Kill status

src/imu.cpp
@@ -1,8 +1,8 @@
 #include "mbed.h"
 #include "imu.h"
 
-IMU::IMU(PinName tx, PinName rx, int baud, Serial* pc) {
-	p_pc = pc;
+IMU::IMU(PinName tx, PinName rx, int baud, PC* pc) {
+	p_pc = pc->p_device;
 	p_device = new Serial(tx, rx);
 	p_device->baud(baud);
 	p_device->format(8, Serial::None, 1);   // serial settings for the IMU--refer to IMU's data sheet
@@ -31,7 +31,7 @@ IMU::IMU(PinName tx, PinName rx, int baud, Serial* pc) {
 	buffer_overflow = false;
 	
 	// Attach it as an RX interrupt only.
-	p_device->attach(&rx_interrupt, Serial::RxIrq);
+	p_device->attach(&rx_interrupt_imu, Serial::RxIrq);
 	p_device->putc('4');	// tell the imu to start sending in case it isn't doing that already.
 }
 
@@ -74,10 +74,6 @@ void IMU::parse() {
 
 bool IMU::readable() {
 	return p_device->readable();
-}
-
-char IMU::getc() {
-	return p_device->getc();
 }
 
 void IMU::getData() {
@@ -201,18 +197,18 @@ void IMU::calcHeading() {
 		
 }
 
-void IMU::rx_interrupt()
+void rx_interrupt_imu()
 {
 	led2 = !led2;
 	
-	while (p_device->readable()) {
-		buffer[i_buffer_write] = p_device->getc();
+	while (imu.p_device->readable()) {
+		imu.buffer[imu.i_buffer_write] = imu.p_device->getc();
 		//pc.putc(imu.buffer[imu.i_buffer_write]);
 		NVIC_DisableIRQ(UART3_IRQn);
-		i_buffer_write = (i_buffer_write + 1) % IMU_RX_BUFFER_SIZE;
+		imu.i_buffer_write = (imu.i_buffer_write + 1) % IMU_RX_BUFFER_SIZE;
 		NVIC_EnableIRQ(UART3_IRQn);
-		if (i_buffer_write == i_buffer_read) {
-			buffer_overflow = true;
+		if (imu.i_buffer_write == imu.i_buffer_read) {
+			imu.buffer_overflow = true;
 			NVIC_DisableIRQ(UART3_IRQn);
 			break;
 		}

src/imu.h
@@ -5,15 +5,21 @@
 #include <string>
 #include <sstream>
 #include <limits.h>
+
 #include "debug.h"
+#include "pc.h"
 
 #define IMU_RX_BUFFER_SIZE 1024
+
+void rx_interrupt_imu();
 
 class IMU {
 // TODO: Figure out which functions and variables can be private.
 public:
-	IMU(PinName tx, PinName rx, int baud, Serial* pc);
+	IMU(PinName tx, PinName rx, int baud, PC* pc);
 	~IMU();
+	
+	friend void rx_interrupt_imu();
 	
 	void putc(char);
 	bool readable();
@@ -52,9 +58,9 @@ private:
 	char linebuf[1024];
 	int i_linebuf;
 	bool buffer_overflow;
-
-	void rx_interupt();
 };
+
+void rx_interrupt();
 
 extern IMU imu;
 

src/motor.cpp
@@ -13,7 +13,7 @@ Motor::Motor(int num_motors, int baud, PinName tx, PinName rx) {
 	p_device->format(8, Serial::None, 1);
 	p_device->baud(baud); // Set the baud.
 	
-	p_device->attach(&tx_interrupt, Serial::TxIrq);
+	p_device->attach(&tx_interrupt_motor, Serial::TxIrq);
 
 	for (int i = 0; i < MOTOR_TX_BUF_SIZE; i++) {
 		buffer[i] = 0;
@@ -69,15 +69,15 @@ char Motor::get(int i_motor) {
 	return motors[i_motor];
 }
 
-void Motor::tx_interrupt(){
-	while (p_device->writeable() && i_buffer_write != i_buffer_read) {
-		p_device->putc(buffer[i_buffer_read]);
-		i_buffer_read = (i_buffer_read + 1) % MOTOR_TX_BUF_SIZE;
-		buffer_empty = false;
+void tx_interrupt_motor() {
+	while (motor.p_device->writeable() && motor.i_buffer_write != motor.i_buffer_read) {
+		motor.p_device->putc(motor.buffer[motor.i_buffer_read]);
+		motor.i_buffer_read = (motor.i_buffer_read + 1) % MOTOR_TX_BUF_SIZE;
+		motor.buffer_empty = false;
 	}
-	if (i_buffer_write == i_buffer_read) {
-		buffer_empty = true;
-		NVIC_DisableIRQ(UART0_IRQn);
+	if (motor.i_buffer_write == motor.i_buffer_read) {
+		motor.buffer_empty = true;
+		NVIC_DisableIRQ(UART1_IRQn);
 		// if nothing to write, turn off the interrupt until motor.getc() is called again
 	}
 }

src/motor.h
@@ -19,6 +19,8 @@
 #define BACK 3
 #endif
 
+void tx_interrupt_motor();
+
 // Stores values for and communicates with the motor controller.
 // We use the Mini SSC II format. This is used to control the motors and move the sub.
 class Motor {
@@ -36,7 +38,7 @@ public:
 	
 	bool buffer_empty;
 
-	void tx_interrupt();
+	friend void tx_interrupt_motor();
 	
 private:
 	int num_motors; // number of motors, counting from 1

src/pc.cpp
@@ -1,7 +1,7 @@
 #include "pc.h"
 
 
-PC::PC(PinName tx, PinName, rx, int baud)
+PC::PC(PinName tx, PinName rx, int baud)
 {
 	p_device = new Serial(tx, rx);
 	p_device->baud(baud);
@@ -12,8 +12,8 @@ PC::PC(PinName tx, PinName, rx, int baud)
 		rx_buffer[i] = tx_buffer[i] = 0;
 	}
 
-	i_tx_read = i_tx_write = 0
-	i_rx_read, i_rx_write = 0
+	i_tx_read = i_tx_write = 0;
+	i_rx_read = i_rx_write = 0;
 
 	tx_empty = true;
 
@@ -29,12 +29,12 @@ PC::PC(PinName tx, PinName, rx, int baud)
 	mes = new char[12];
 	strcpy(mes, "h||d||p|||\n");
 
-	p_device->attach(&rx_interrupt, Serial::RxIrq);
-	p_device->attach(&tx_interrupt, Serial::TxIrq);
+	p_device->attach(&rx_interrupt_pc, Serial::RxIrq);
+	p_device->attach(&tx_interrupt_pc, Serial::TxIrq);
 
 	if (!debug) {
 		pc_ticker = new Ticker();
-		ticker_pc->attach(&send_status, 1.0);
+		pc_ticker->attach(&send_status_pc, 1.0);
 	}
 }
 
@@ -64,32 +64,32 @@ int PC::decode_avnav(avnav data) {
 }
 
 
-void PC::send_status()
+void send_status_pc()
 {
 	avnav avnav_temp;
 
 	// Construct the message to the BeagleBoard based on state of the sub.
 	// get heading value
-	avnav_temp = encode_avnav(calcH);
-	mes[1] = avnav_temp.byte1;
-	mes[2] = avnav_temp.byte2;
+	avnav_temp = pc.encode_avnav(calcH);
+	pc.mes[1] = avnav_temp.byte1;
+	pc.mes[2] = avnav_temp.byte2;
 	
 	// get depth (pressure sensor) value
-	avnav_temp = encode_avnav(pressure.getValueCalibrated());
-	mes[4] = avnav_temp.byte1;
-	mes[5] = avnav_temp.byte2;
+	avnav_temp = pc.encode_avnav(pressure.getValueCalibrated());
+	pc.mes[4] = avnav_temp.byte1;
+	pc.mes[5] = avnav_temp.byte2;
 	
 	// get power (motor) value
-	avnav_temp = encode_avnav(desPower);
-	mes[7] = avnav_temp.byte1;
-	mes[8] = avnav_temp.byte2;
+	avnav_temp = pc.encode_avnav(desPower);
+	pc.mes[7] = avnav_temp.byte1;
+	pc.mes[8] = avnav_temp.byte2;
 	
 	// get kill switch value
 	// TODO: is this correct? I assume voltage on kill pin means alive.
-	mes[9] = kill.getValueThresh() ? 'k' : 'l';
+	pc.mes[9] = kill.getValueThresh() ? 'k' : 'l';
 	
 	// print message to PC
-	send_message(mes);
+	pc.send_message(pc.mes);
 }
 
 void PC::send_message(const char* message)
@@ -110,29 +110,29 @@ void PC::putc(char c)
 	// Don't worry about overflow because if you're 1024 chars behind you're FUBAR already
 }
 
-void PC::tx_interrupt()
+void tx_interrupt_pc()
 {
-	while (p_device->writeable() && i_tx_write != i_tx_read) {
-		p_device->putc(buffer[i_tx_read]);
-		i_tx_read = (i_tx_read + 1) % PC_BUFFER_SIZE;
-		tx_empty = false;
+	while (pc.p_device->writeable() && pc.i_tx_write != pc.i_tx_read) {
+		pc.p_device->putc(pc.tx_buffer[pc.i_tx_read]);
+		pc.i_tx_read = (pc.i_tx_read + 1) % PC_BUFFER_SIZE;
+		pc.tx_empty = false;
 	}
-	if (i_tx_write == i_tx_read) {
-		tx_empty = true;
+	if (pc.i_tx_write == pc.i_tx_read) {
+		pc.tx_empty = true;
 		NVIC_DisableIRQ(UART1_IRQn);
 		// if nothing to write, turn off the interrupt until motor.getc() is called again
 	}
 }
 
-void PC::rx_interrupt()
+void rx_interrupt_pc()
 {
-	while (p_device->readable()) {
-		rx_buffer[i_rx_write] = p_device->getc();
+	while (pc.p_device->readable()) {
+		pc.rx_buffer[pc.i_rx_write] = pc.p_device->getc();
 		NVIC_DisableIRQ(UART0_IRQn);
-		i_buffer_write = (i_buffer_write + 1) % IMU_RX_BUFFER_SIZE;
+		pc.i_rx_write = (pc.i_rx_write + 1) % PC_BUFFER_SIZE;
 		NVIC_EnableIRQ(UART0_IRQn);
-		if (i_rx_write == i_rx_read) {
-			rx_overflow = true;
+		if (pc.i_rx_write == pc.i_rx_read) {
+			pc.rx_overflow = true;
 			NVIC_DisableIRQ(UART0_IRQn);
 			break;
 		}
@@ -155,7 +155,7 @@ char PC::readPC()
 			((i_rx_read + 1) % PC_BUFFER_SIZE) != i_rx_write &&		//make sure there are 4 characters to be read
 			((i_rx_read + 2) % PC_BUFFER_SIZE) != i_rx_write &&
 			((i_rx_read + 3) % PC_BUFFER_SIZE) != i_rx_write &&
-			(rx_buffer[(i_rx_read + 3) % PC_BUFFER_SIZE] == '\n') || //and the fourth character is a newline
+			(rx_buffer[(i_rx_read + 3) % PC_BUFFER_SIZE] == '\n')) || //and the fourth character is a newline
 			rx_overflow) { 
 		rx_overflow = false;
 		avnav temp;

src/pc.h
@@ -13,7 +13,9 @@ struct avnav {
 	char byte2;
 };
 
-
+void rx_interrupt_pc();
+void tx_interrupt_pc();
+void send_status_pc();
 
 class PC {
 public:
@@ -21,7 +23,6 @@ public:
 
 	avnav encode_avnav(int data);
 	int decode_avnav(avnav data);
-	void send_status();
 	void send_message(const char* message);
 
 	void putc(char c); //add c to the write FIFO
@@ -36,18 +37,18 @@ public:
 	bool tx_empty;
 	bool rx_overflow;
 
-	void tx_interrupt();
 private:
-
+	friend void rx_interrupt_pc();
+	friend void tx_interrupt_pc();
+	friend void send_status_pc();
+	
 	char* mes;
 	int message_length;
 	char tx_buffer[PC_BUFFER_SIZE], rx_buffer[PC_BUFFER_SIZE];
 	int i_tx_read, i_tx_write;
 	int i_rx_read, i_rx_write;
-	
-	void rx_interrupt();
 
-}
+};
 
 extern bool debug;
 extern PC pc;

src/pid.h
@@ -74,8 +74,6 @@ extern float depth;
 
 extern unsigned char motorArray[4];
 
-extern Serial pc;
-
 void reset_pid();
 void init_pid();
 void do_pid();